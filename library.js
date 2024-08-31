1fb2dddedeca5b89d440fd3db775df67cc1df7d1
Dixit Sathwara <dixitgsathwara1708@gmail.com>
2024-08-31 12:35:09 +0530

    implimented code to check book isbn number match all condition

--- library.js
+++ library.js
@@ -6,6 +6,12 @@ class Library{
         if (!book.isbn || !book.title || !book.author || !book.publicationYear) {
             throw new Error('All fields (ISBN, title, author, publicationYear) are required.');
         }
+        if (this.books.some(b => b.isbn === book.isbn)){
+            throw new Error('The same ISBN number book is already present.');
+        }
+        if(book.isbn < 99999){
+            throw new Error('The ISBN number length should be greater than 5');
+        }
         this.books.push(book);
         return this.books;
     }
