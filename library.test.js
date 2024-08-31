a74ff02a6c6169b31a83e4ae29554a1c7df3b511
Dixit Sathwara <dixitgsathwara1708@gmail.com>
2024-08-31 11:46:36 +0530

    Added failing test cases for addBooks

--- library.test.js
+++ library.test.js
@@ -1,5 +1,5 @@
 const Library=require('./library')
-
+const Book=require('./book')
 describe("Library Management System", () => {
     let library;
 
@@ -9,5 +9,11 @@ describe("Library Management System", () => {
 
     test('should initialize a library with an empty books array', () => {
         expect(library.books).toEqual([]); 
+    });
+
+    test("should add and return added book", () => {
+        let book1 = new Book(123456, 'Introduction to the Theory of Computation', 'Michael Sipser', 2012);
+        library.addBook(book1);
+        expect(library.books).toContain(book1);
     });
 });
