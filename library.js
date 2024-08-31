a4f83d6e0f64554322220e6e77370dbfda2338ec
Dixit Sathwara <dixitgsathwara1708@gmail.com>
2024-08-31 12:08:54 +0530

    implimented code to check all the fields in book are not empty

--- library.js
+++ library.js
@@ -3,6 +3,9 @@ class Library{
         this.books=[];
     }
     addBook(book){
+        if (!book.isbn || !book.title || !book.author || !book.publicationYear) {
+            throw new Error('All fields (ISBN, title, author, publicationYear) are required.');
+        }
         this.books.push(book);
         return this.books;
     }
